9dddce19e33fad45e171b464edd0176db0b57b65
Noaman67khan <92851833+Noaman67khan@users.noreply.github.com>
2021-10-28 02:08:51 +0200

    Update example_spai.m

--- example_spai.m
+++ example_spai.m
@@ -1,4 +1,4 @@
-A=load('orsirr_2.m');
+A=load('fs_183_1.m');
 [m,n]=size(A);
 for i=1:m
     j=A(i,1:2);
@@ -7,5 +7,5 @@ end
 A=we;
 n=length(A);
 b=ones(n,1);
-sir3(A,b,0.3,30,30,1,1,2,10);
-%gmresir3(A,b,0.5,30,30,1,1,2,10,1e-04);
+%sir3(A,b,0.3,30,30,1,1,2,10);
+gmresir3(A,b,0.1,17,17,1,1,2,10,1e-04);
