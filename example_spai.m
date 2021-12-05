14a6f93fc631653175d8d81f4ce13c60f766a97a
Noaman67khan <92851833+Noaman67khan@users.noreply.github.com>
2021-12-05 19:54:09 +0100

    Update example_spai.m

--- example_spai.m
+++ example_spai.m
@@ -1,11 +1,8 @@
-A=load('fs_183_1.m');
-[m,n]=size(A);
-for i=1:m
-    j=A(i,1:2);
-    we(j(1),j(2))=A(i,3);
-end
-A=we;
+clc;
+clear all
+data=load('fs_183_1.mtx');
+A = spconvert(data);
 n=length(A);
 b=ones(n,1);
-%sir3(A,b,0.3,30,30,1,1,2,10);
-gmresir3(A,b,0.1,17,17,1,1,2,10,1e-04);
+x = gmresir3(full(A), zeros(n,1), ones(n,1), .5, 20, 20, 1, 2, 4, 15, 1e-6,5,'fs_183_1e05');
+drawnow
