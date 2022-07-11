5c243d3618bd4b1c868a54cb394e102d3659b6f8
Noaman67khan <92851833+Noaman67khan@users.noreply.github.com>
2022-07-11 15:47:24 +0200

    Add files via upload

---
+++ generateplots.m
@@ -0,0 +1,36 @@
+%Generate Plot Example script for comparing GMRES-IR with LU, GMRESIR_SPAI with GMRESIR_NP (with 3 precisions)
+%   Note: Requires Advanpix multiprecision toolbox
+clc;
+clear all
+data=load('jpwh_991');
+A = spconvert(data.Problem.A);
+%A=gallery('poisson',10);
+n=length(A);
+b=ones(n,1);
+     %Run GMRES-IR with half,single,double
+     fprintf('Running GMRES-IR\n');
+     gmresir3(full(A),zeros(n,1),ones(n,1),0,1,2,15,1e-4);
+     %Run GMRES-IR with single,single,double
+     gmresir3(full(A),zeros(n,1),ones(n,1),1,1,2,15,1e-4);
+     %Run GMRES-IR with single,double,quad
+     gmresir3(full(A),zeros(n,1),ones(n,1),1,2,4,15,1e-4);
+     axis([0 5 10.^(-30) 10.^(5)]);
+     drawnow
+     %Run GMRESIR_SPAI with half,single,double
+     fprintf('Running GMRESIR_SPAI\n');
+     gmresir3_spai(full(A), zeros(n,1), ones(n,1), .2, 50, 50, 0, 1, 2, 15, 1e-4);
+     %Run GMRESIR_SPAI with single,single,double
+     gmresir3_spai(full(A), zeros(n,1), ones(n,1), .2, 50, 50, 1, 1, 2, 15, 1e-4);
+     %Run GMRESIR_SPAI with single,double,quad
+     gmresir3_spai(full(A), zeros(n,1), ones(n,1), .2, 50, 50, 1, 2, 4, 15, 1e-4);
+     axis([0 5 10.^(-30) 10.^(5)]);
+     drawnow
+     %Run GMRESIR_NP with half,single,double
+     fprintf('Running GMRESIR_np\n');
+     gmresir3_np(full(A), zeros(n,1), ones(n,1), 0, 1, 2, 15, 1e-4);
+     %Run GMRESIR_NP with single,single,double
+     gmresir3_np(full(A), zeros(n,1), ones(n,1), 1, 1, 2, 15, 1e-4);
+     %Run GMRESIR_NP with single,double,quad
+     gmresir3_np(full(A), zeros(n,1), ones(n,1), 1, 2, 4, 15, 1e-4);
+     axis([0 5 10.^(-30) 10.^(5)]);
+     drawnow
