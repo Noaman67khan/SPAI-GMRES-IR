352de8f5db0ac6005f77ee9f76d9c751ad03f341
Noaman67khan <92851833+Noaman67khan@users.noreply.github.com>
2022-07-12 09:18:40 +0200

    Delete example_spai.m

--- example_spai.m
+++
@@ -1,8 +0,0 @@
-clc;
-clear all
-data=load('fs_183_1.mtx');
-A = spconvert(data);
-n=length(A);
-b=ones(n,1);
-x = gmresir3(full(A), zeros(n,1), ones(n,1), .5, 20, 20, 1, 2, 4, 15, 1e-6,5,'fs_183_1e05');
-drawnow
